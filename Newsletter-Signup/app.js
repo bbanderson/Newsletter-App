b906e1aa2757a0fbe505079251deb0c89ff12407
bbanderson <tombyun@naver.com>
2020-05-09 21:31:33 +0900

    Add subscribe data on JSON

--- Newsletter-Signup/app.js
+++ Newsletter-Signup/app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const request = require("request");
+const https = require("https");
 
 const app = express();
 app.use(bodyParser.urlencoded({extended: true}))
@@ -18,6 +19,33 @@ app.post("/", (req, res) => {
     console.log("POST");
     console.log(firstName, lastName, email);
     
+    const data = {
+        members: [
+            {
+                email_address: email,
+                status: "subscribed",
+                merge_fields: {
+                    FNAME: firstName,
+                    LNAME: lastName,
+                }
+            }
+        ]
+    }
+
+    const jsonData = JSON.stringify(data)
+    const url = "https://us18.api.mailchimp.com/3.0/lists/4e4ffa9c39"
+    const options = {
+        method: "POST",
+        auth: "bbanderson:f1c409cb8daa8b5c154f19facfe4c56b-us18"
+    }
+    const request = https.request(url, options, function(response) {
+        response.on("data", data => {
+            console.log(JSON.parse(data));
+        })
+    })
+
+    request.write(jsonData)
+    request.end()
     
 })
 
